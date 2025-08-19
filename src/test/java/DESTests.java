0bb4aa30fab7fa96c590702c9fbe4b0a7d6bf2a6
whwlsfb <whwlsfb@wanghw.cn>
2025-08-19 14:40:46 +0800

    update tests code.

--- src/test/java/DESTests.java
+++ src/test/java/DESTests.java
@@ -1,3 +1,5 @@
+import cn.wanghw.models.BruteResult;
+import cn.wanghw.models.ViewStateData;
 import org.junit.Test;
 
 import java.util.Base64;
@@ -10,16 +12,18 @@ public class DESTests {
 
     @Test
     public void testDESWithEncrypted() throws Exception {
-        String val = Base64.getEncoder().encodeToString(verifyAndDecryptViewState(
-                "Z+rfkSyLKkDpwMiL7PrIvjZHqJ7uqXJHTKJlQgtRiLqkIh0Om2vBNriltBSoGguSvLQe4xgQHA+ph7BuaEbcCwsNWFdN0V07ivvk0kJ324YyJ3W35It8uk1loJ4/4cAL",
-                "319B474B1D2B7A87C996B280450BB36506A95AEDF9B51211",
-                "HMACSHA512",
-                "280450BB36319B474C996B506A95AEDF9B51211B1D2B7A87",
-                "DES",
-                "5A108D17",
-                true, ""));
-        if (!val.equals("/wEPDwUKMTY3NzE5MjIyMGRk"))
-            throw new Exception(val);
+        ViewStateData viewStateData = new ViewStateData();
+        viewStateData.setViewState("Z+rfkSyLKkDpwMiL7PrIvjZHqJ7uqXJHTKJlQgtRiLqkIh0Om2vBNriltBSoGguSvLQe4xgQHA+ph7BuaEbcCwsNWFdN0V07ivvk0kJ324YyJ3W35It8uk1loJ4/4cAL");
+        viewStateData.setViewStateGenerator("5A108D17");
+        viewStateData.setViewStateEncrypted(true);
+        BruteResult bruteResult = new BruteResult();
+        bruteResult.setValidationAlgorithm("HMACSHA512");
+        bruteResult.setValidationKeyHex("319B474B1D2B7A87C996B280450BB36506A95AEDF9B51211");
+        bruteResult.setDecryptionAlgorithm("DES");
+        bruteResult.setDecryptionKeyHex("280450BB36319B474C996B506A95AEDF9B51211B1D2B7A87");
+        verifyAndDecryptViewState(viewStateData, bruteResult);
+        if (!bruteResult.isSuccess())
+            throw new Exception(bruteResult.getLosFormatterPayload());
         else {
             logger.info("DESWithEncrypted ok!");
         }
